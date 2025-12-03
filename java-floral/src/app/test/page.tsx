10b09380c80d10e758e1d305d3242a3cb121b9b1
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-12-03 17:41:00 +0700

    add components

--- java-floral/src/app/test/page.tsx
+++ java-floral/src/app/test/page.tsx
@@ -178,8 +178,8 @@ export default function Page({}) {
             </FieldSet>
           </FieldGroup>
         </form>
-        <section className="px-8 lg:pl-4">
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <section className="flex-1 px-8 lg:pl-4">
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
             {Array.from({ length: 10 }).map((_, index) => (
               <Card key={index} className="rounded-md">
                 <CardThumbnail>
--- java-floral/src/components/ui/header.tsx
+++ java-floral/src/components/ui/header.tsx
@@ -5,6 +5,10 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import "tailwindcss";
 import { Signature } from "./signature";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "./navigation-menu";
+import { Button } from "./button";
+import { SearchIcon, ShoppingBasketIcon, UserIcon } from "lucide-react";
+import { Badge } from "./badge";
 
 export function Header({}) {
 
@@ -18,14 +22,10 @@ export function Header({}) {
     }
 
     useEffect(() => {
-
-        
         window.addEventListener("scroll", handleDocumentScroll);
-        console.log("registered scroll event");
 
         return () => {
             window.removeEventListener("scroll", handleDocumentScroll);
-            console.log("unregistered scroll event");
         }
     }, [])
 
@@ -36,7 +36,7 @@ export function Header({}) {
                 isScrolled ? "border-border bg-background" : ""
             )}>
                 <div className={cn(
-                    "container mx-auto flex items-center h-24 px-8 transition-all duration-300",
+                    "container mx-auto flex items-center h-24 px-8 transition-all duration-300 relative",
                     isScrolled ? "h-16" : ""
                 )}>
                     <Link href="/">
@@ -44,6 +44,125 @@ export function Header({}) {
                             <Signature />
                         </h1>
                     </Link>
+                    <NavigationMenu className="flex-1 ml-4 max-w-none *:first:w-full">
+                        <NavigationMenuList className="flex-wrap">
+                            <NavigationMenuItem>
+                                <NavigationMenuTrigger>Bouquets</NavigationMenuTrigger>
+                                <NavigationMenuContent>
+                                    <div className="flex gap-4 w-max max-w-dvw">
+                                        <div className="flex flex-col">
+                                            <h3 className="font-heading px-2 py-1 text-2xl">For those special moments</h3>
+                                            <NavigationMenuLink asChild>
+                                                <Link href="/">
+                                                    Birthdays
+                                                </Link>
+                                            </NavigationMenuLink>
+                                            <NavigationMenuLink asChild>
+                                                <Link href="/">
+                                                    Anniversaries
+                                                </Link>
+                                            </NavigationMenuLink>
+                                            <NavigationMenuLink asChild>
+                                                <Link href="/">
+                                                    Weddings
+                                                </Link>
+                                            </NavigationMenuLink>
+                                            <NavigationMenuLink asChild>
+                                                <Link className="text-muted-foreground italic" href="/">
+                                                    More occasions...
+                                                </Link>
+                                            </NavigationMenuLink>
+                                        </div>
+                                    </div>
+                                </NavigationMenuContent>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem>
+                                <NavigationMenuTrigger>Flowers</NavigationMenuTrigger>
+                                <NavigationMenuContent>
+                                    <div className="flex gap-4 w-max max-w-dvw">
+                                        <div className="flex flex-col">
+                                            <h3 className="font-heading px-2 py-1 text-2xl">Browse our garden</h3>
+                                            <NavigationMenuLink asChild>
+                                                <Link href="/">
+                                                    Flowers for tea
+                                                </Link>
+                                            </NavigationMenuLink>
+                                        </div>
+                                        <div className="flex flex-col">
+                                            <h3 className="font-heading px-2 py-1 text-2xl">By species</h3>
+                                            <NavigationMenuLink asChild>
+                                                <Link href="/">
+                                                    Daisies
+                                                </Link>
+                                            </NavigationMenuLink>
+                                            <NavigationMenuLink asChild>
+                                                <Link href="/">
+                                                    Roses
+                                                </Link>
+                                            </NavigationMenuLink>
+                                            <NavigationMenuLink asChild>
+                                                <Link href="/">
+                                                    Chrysanthemums
+                                                </Link>
+                                            </NavigationMenuLink>
+                                            <NavigationMenuLink asChild>
+                                                <Link className="text-muted-foreground italic" href="/">
+                                                    More species...
+                                                </Link>
+                                            </NavigationMenuLink>
+                                        </div>
+                                    </div>
+                                </NavigationMenuContent>
+                            </NavigationMenuItem>
+                            <span className="grow"></span>
+                            <Button variant="ghost" className="rounded-full size-10 p-0">
+                                <SearchIcon className="size-5" />
+                            </Button>
+                            <NavigationMenuItem>
+                                <NavigationMenuTrigger className="rounded-full size-10 p-0" hasIcon={false}>
+                                    <ShoppingBasketIcon className="size-5" />
+                                    <span>
+                                        <Badge variant="secondary" className="absolute -bottom-1 -right-1 h-5 min-w-5 p-1 tabular-nums">
+                                            0
+                                        </Badge>
+                                    </span>
+                                </NavigationMenuTrigger>
+                                <NavigationMenuContent>
+                                    <div className="flex md:justify-end md:text-end gap-4 w-full">
+                                        <div className="flex flex-col">
+                                            <h3 className="font-heading px-2 py-1 text-2xl">Basket</h3>
+                                        </div>
+                                    </div>
+                                </NavigationMenuContent>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem className="ml-2">
+                                <NavigationMenuTrigger className="border rounded-full size-12 p-0" hasIcon={false}>
+                                    <UserIcon className="size-5" />
+                                </NavigationMenuTrigger>
+                                <NavigationMenuContent>
+                                    <div className="flex md:justify-end md:text-end gap-4 w-full">
+                                        <div className="px-2 text-sm">
+                                            <p className="max-w-80">
+                                                Sign up for an account and get extra features and exclusive discounts!
+                                            </p>
+                                            <p className="flex md:flex-row-reverse mt-4 gap-2">
+                                                <Button variant="secondary" asChild>
+                                                    <Link href="/login">
+                                                        Log in
+                                                    </Link>
+                                                </Button>
+                                                <Button asChild>
+                                                    <Link href="/register">
+                                                        Create an account
+                                                    </Link>
+                                                </Button>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </NavigationMenuContent>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenu>
                 </div>
             </div>
         </header>
