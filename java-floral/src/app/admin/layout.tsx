9699e717903190f8f41f71377f01e308d1169095
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-12-03 19:04:46 +0700

    add admin panel

--- java-floral/src/app/admin/layout.tsx
+++ java-floral/src/app/admin/layout.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { AdminSidebar } from "@/components/blocks/layout/admin-sidebar";
 import { Root } from "@/components/layouts/root"
 import { AdminContext } from "@/components/providers/contexts/admin-context"
 import { Providers } from "@/components/providers/providers"
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { ReactNode } from "react"
 
 export default function Layout({ children }: { 
@@ -10,7 +12,10 @@ export default function Layout({ children }: {
 }) {
   return (
     <AdminContext value={{}}>
-        <main>{children}</main>
+        <SidebarProvider>
+            <AdminSidebar />
+            <main>{children}</main>
+        </SidebarProvider>
     </AdminContext>
   )
 }
