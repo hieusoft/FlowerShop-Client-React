db108f1afca0cdcf9b476a28f4cd4b6699b9f7fa
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-12-08 18:18:41 +0700

    add data table 2

--- src/components/blocks/dashboard/data-manager.tsx
+++ src/components/blocks/dashboard/data-manager.tsx
@@ -4,6 +4,9 @@ import { ColumnDef } from "@tanstack/react-table";
 import { useState } from "react";
 import { DataTable } from "./data-table";
 import { createContext } from "vm";
+import { Button } from "@/components/ui/button";
+import { ChevronLastIcon, ChevronLeftIcon, ChevronRightIcon, ChevronsLeftIcon, ChevronsRightIcon } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 export function DataManager<TData, TValue>(
     { columns }: {
@@ -12,9 +15,34 @@ export function DataManager<TData, TValue>(
 ) {
     const [data, setData] = useState<TData[]>([]);
 
+    const [currentPage, setCurrentPage] = useState(1);
+    const [maxPages, setMaxPages] = useState(1);
+
     return <>
         <DataTable 
-            className="flex-1 -m-4 mb-0 border-b"
+            className="flex-1 -m-4 mb-0"
             data={data} columns={columns} />
+        <div className="flex flex-row items-center gap-2 -m-4 p-4 mt-4 border-t bg-popover text-popover-foreground">
+            <span className="flex-1"></span>
+            <span className="block px-2 ml-3 lining-nums">
+                {currentPage} / {maxPages}
+            </span>
+            <Button variant="outline" size="icon">
+                <ChevronsLeftIcon />
+                <span className="sr-only">First</span>
+            </Button>
+            <Button variant="outline" size="icon">
+                <ChevronLeftIcon />
+                <span className="sr-only">Previous</span>
+            </Button>
+            <Button variant="outline" size="icon">
+                <ChevronRightIcon />
+                <span className="sr-only">Next</span>
+            </Button>
+            <Button variant="outline" size="icon">
+                <ChevronsRightIcon />
+                <span className="sr-only">Last</span>
+            </Button>
+        </div>
     </>
 } 
